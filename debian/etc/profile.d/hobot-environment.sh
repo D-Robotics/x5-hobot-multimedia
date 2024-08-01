dbfcab85bd6e279a70096ea924d3083c35a57c99
xin03.liu <xin03.liu@horizon.cc>
2024-08-01 19:32:22 +0800

    feat(LD_LIBRARY_PATH): Add LD_LIBRARY_PATH
    Signed-off-by: xin03.liu <xin03.liu@horizon.cc>

--- debian/etc/profile.d/hobot-environment.sh
+++ debian/etc/profile.d/hobot-environment.sh
@@ -1,2 +1,3 @@
 export PATH="/usr/hobot/bin/:${PATH}"
+export LD_LIBRARY_PATH="/usr/hobot/lib:/app/lib:/app/pub/lib:/middleware/lib:/middleware/pub/lib:/usr/hobot/lib/sensor:/system/lib:/system/usr/lib:/lib:${LD_LIBRARY_PATH}"
 
